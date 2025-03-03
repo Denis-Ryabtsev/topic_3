357aac52ed4e5e97ef6ce870218c7ea8038deba9
lazarus <you@example.com>
2025-03-03 15:01:40 +0700

    Add: added ViewSet for breed and dogs

--- animals/dogs/views.py
+++ animals/dogs/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from dogs.models import Dog, Breed
+from dogs.serializers import DogSerializer, BreedSerializer
+
+
+class DogViewSet(viewsets.ModelViewSet):
+    queryset = Dog.objects.all()
+    serializer_class = DogSerializer
+
+
+class BreedViewSet(viewsets.ModelViewSet):
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
