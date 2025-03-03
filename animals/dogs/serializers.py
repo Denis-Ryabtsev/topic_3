5bd43f4f9b03593bb9a2b6727231f80a1e702a13
lazarus <you@example.com>
2025-03-03 15:03:03 +0700

    Add: added serializers for dog, breed models

---
+++ animals/dogs/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from dogs.models import Dog, Breed
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = '__all__'
+
+
+class DogSerializer(serializers.ModelSerializer):
+    breed = serializers.PrimaryKeyRelatedField(
+        queryset=Breed.objects.all()
+    )
+
+    class Meta:
+        model = Dog
+        fields = '__all__'
